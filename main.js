0e3fce719cbae7c847a910e2c1f1db2652ff0111
GabiAmpese <ampesegabriela@gmail.com>
2025-10-02 15:27:28 -0300

    Adiciona tela inicial com nome, redes e apresentação.

---
+++ main.js
@@ -0,0 +1,8 @@
+// Animação de digitação usando Typed.js
+var typed = new Typed(".typing", {
+  strings: ["Estudante", "Programadora", "Designer", "Desenvolvedora"], // palavras que vão aparecer
+  typeSpeed: 120,   // velocidade de digitação
+  backSpeed: 65,    // velocidade de apagar
+  loop: true        // repetir para sempre
+});
+
