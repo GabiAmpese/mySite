8f0536a4c9ab30f491cf5f3fc870601d04c77ad5
GabiAmpese <ampesegabriela@gmail.com>
2025-10-03 10:00:22 -0300

    Incluindo as sessões que ao clicar, abrem o conteúdo sobre mim.

--- main.js
+++
@@ -1,8 +0,0 @@
-// Animação de digitação usando Typed.js
-var typed = new Typed(".typing", {
-  strings: ["Estudante", "Programadora", "Designer", "Desenvolvedora"], // palavras que vão aparecer
-  typeSpeed: 120,   // velocidade de digitação
-  backSpeed: 65,    // velocidade de apagar
-  loop: true        // repetir para sempre
-});
-
