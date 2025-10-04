4a84c67c28692b5dd0b470287a73a63251b50dcc
GabiAmpese <ampesegabriela@gmail.com>
2025-10-03 21:37:11 -0300

    Limpando o código

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-// Animação de digitação usando Typed.js
 var typed = new Typed(".typing", {
   strings: ["Estudante", "Programadora", "Designer", "Desenvolvedora"], 
-  typeSpeed: 120,   // velocidade de digitação
-  backSpeed: 65,    // velocidade de apagar
-  loop: true        // repetir para sempre
+  typeSpeed: 120,  
+  backSpeed: 65,   
+  loop: true        
 });
 
-// Função para abrir abas
 function abrirAba(id) {
-  // esconde todas
   document.querySelectorAll('.aba').forEach(div => div.style.display = 'none');
-  // mostra só a clicada
   document.getElementById(id).style.display = 'block';
 }
-
